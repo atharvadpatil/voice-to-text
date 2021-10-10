bb2babd31914e7724f8aa7a60be1ec416d08c159
Atharva Patil <atharva.dp@somaiya.edu>
2021-10-10 18:32:38 +0530

    basic layout done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
+      <div className="box">
+        <h2>Current Note</h2>
+        <button>Start/Stop</button>
+        <button>Save Note</button>
+      </div>
+      <div className="box">
+        <h2>Saved Notes</h2>
+      </div>
     </div>
   );
 }
